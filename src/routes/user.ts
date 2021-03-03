6be3faa30b605e7724cd9667c47589d7b2a0c2cf
Aigars Peda <aigarspeda@gmail.com>
2021-03-03 10:04:50 +0200

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,7 @@ import controller from "../controllers/user";
 const router = express.Router();
 
 router.post("/create/user", controller.createUser);
+router.post("/login/user", controller.loginUser);
 // router.get('/get/books', controller.getAllBooks);
 
 export = router;
