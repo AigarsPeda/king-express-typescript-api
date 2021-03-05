b72f66500d80205347f3a89d45b5ace4b734af80
Aigars Peda <aigarspeda@gmail.com>
2021-03-05 07:39:05 +0200

    Update database.sql

--- database.sql
+++ database.sql
@@ -7,5 +7,18 @@ CREATE TABLE IF NOT EXISTS users (
   password VARCHAR ( 255 ) NOT NULL,
   email VARCHAR ( 255 ) UNIQUE NOT NULL,
   created_on TIMESTAMP NOT NULL,
-  last_login TIMESTAMP 
+  last_login TIMESTAMP,
+  games_played INTEGER NOT NULL DEFAULT 0,
+  games_won INTEGER NOT NULL DEFAULT 0,
+  games_lost INTEGER NOT NULL DEFAULT 0
+);
+
+
+CREATE TABLE IF NOT EXISTS games (
+  game_id serial PRIMARY KEY,
+  game_creator_id INTEGER NOT NULL,
+  game_created_on TIMESTAMP NOT NULL,
+  game_ended_on TIMESTAMP,
+  player_array JSONB,
+  FOREIGN KEY (game_creator_id) REFERENCES users (user_id)
 );
