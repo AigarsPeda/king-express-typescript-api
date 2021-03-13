03cd2d94d98934daacdb53cc6abbe127c5037544
Aigars Peda <aigarspeda@gmail.com>
2021-03-13 17:37:46 +0200

    Update user.ts

--- src/routes/user.ts
+++ src/routes/user.ts
@@ -4,6 +4,7 @@ import controller from "../controllers/user";
 const router = express.Router();
 
 router.put("/user", controller.updateUser);
+router.delete("/user/:user_id_param", controller.deleteUser);
 
 // router.get('/get/books', controller.getAllBooks);
 
