58b6358894853859643496f109b468d3787ef01f
Aigars Peda <aigarspeda@gmail.com>
2021-04-22 07:25:57 +0300

    Update database.sql

--- database.sql
+++ database.sql
@@ -20,14 +20,22 @@ CREATE TABLE IF NOT EXISTS users_stats (
   tournaments_created INTEGER NOT NULL DEFAULT 0,
   user_id INTEGER NOT NULL,
   FOREIGN KEY (user_id) REFERENCES users (user_id)
-)
+);
 
+CREATE TABLE IF NOT EXISTS players (
+  player_id serial PRIMARY KEY,
+  points INTEGER NOT NULL DEFAULT 0,
+  tournament_won INTEGER NOT NULL DEFAULT 0,
+  tournament_lost INTEGER NOT NULL DEFAULT 0,
+  in_tournament_id INTEGER NOT NULL,
+  tournament_id INTEGER NOT NULL,
+  FOREIGN KEY (tournament_id) REFERENCES users (tournament_id)
+)
 
 CREATE TABLE IF NOT EXISTS tournaments (
   tournament_id serial PRIMARY KEY,
   tournament_creator_id INTEGER NOT NULL,
   tournament_created_on TIMESTAMP NOT NULL,
   tournament_ended_on TIMESTAMP,
-  player_array JSONB,
   FOREIGN KEY (tournament_creator_id) REFERENCES users (user_id)
-)
+);
