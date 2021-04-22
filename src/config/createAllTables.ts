582848989140aa64b721d32aa89adc293373e214
Aigars Peda <aigarspeda@gmail.com>
2021-04-22 07:26:16 +0300

    Create createAllTables.ts

---
+++ src/config/createAllTables.ts
@@ -0,0 +1,64 @@
+import logging from "./logging";
+import { poll } from "./postgresql";
+const NAMESPACE = "Table creation";
+
+export const createAllTables = async () => {
+  logging.info(NAMESPACE, "Starting to create all tables");
+  await poll.query(
+    `
+      CREATE TABLE IF NOT EXISTS users (
+        user_id serial PRIMARY KEY,
+        name VARCHAR ( 50 ) NOT NULL,
+        surname VARCHAR ( 50 ) NOT NULL,
+        password VARCHAR ( 255 ) NOT NULL,
+        email VARCHAR ( 255 ) UNIQUE NOT NULL,
+        created_on TIMESTAMP NOT NULL,
+        last_login TIMESTAMP,
+        agree_to_terms BOOLEAN NOT NULL
+      )
+    `
+  );
+
+  await poll.query(
+    `
+      CREATE TABLE IF NOT EXISTS users_stats (
+        stats_id serial PRIMARY KEY,
+        points_overall INTEGER NOT NULL DEFAULT 0,
+        tournaments_played INTEGER NOT NULL DEFAULT 0,
+        tournaments_won INTEGER NOT NULL DEFAULT 0,
+        tournaments_lost INTEGER NOT NULL DEFAULT 0,
+        tournaments_created INTEGER NOT NULL DEFAULT 0,
+        user_id INTEGER NOT NULL,
+        FOREIGN KEY (user_id) REFERENCES users (user_id)
+      )
+    `
+  );
+
+  await poll.query(
+    `
+      CREATE TABLE IF NOT EXISTS tournaments (
+        tournament_id serial PRIMARY KEY,
+        tournament_creator_id INTEGER NOT NULL,
+        tournament_created_on TIMESTAMP NOT NULL,
+        tournament_winner INTEGER,
+        tournament_ended_on TIMESTAMP,
+        FOREIGN KEY (tournament_creator_id) REFERENCES users (user_id)
+      )
+    `
+  );
+
+  await poll.query(
+    `
+      CREATE TABLE IF NOT EXISTS players (
+        player_id serial PRIMARY KEY,
+        name VARCHAR ( 50 ) NOT NULL,
+        in_tournament_id INTEGER NOT NULL,
+        points INTEGER NOT NULL DEFAULT 0,
+        isWinner BOOLEAN DEFAULT false,
+        tournament_id INTEGER NOT NULL,
+        FOREIGN KEY (tournament_id) REFERENCES tournaments (tournament_id)
+      )
+    `
+  );
+  logging.info(NAMESPACE, "Finished creating all tables");
+};
