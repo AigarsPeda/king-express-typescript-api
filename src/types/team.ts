1f674433cb125d4b5d411f24f4c0e5c13ce364d6
Aigars Peda <aigarspeda@gmail.com>
2021-04-27 08:59:05 +0300

    Update team.ts

--- src/types/team.ts
+++ src/types/team.ts
@@ -3,4 +3,16 @@ export type ITeam = {
   secondPlayer: string;
   team: number;
   score: number;
+  winner: boolean;
+};
+
+export type ITeamDB = {
+  team_id: number;
+  game_number: number;
+  player_1: string;
+  player_2: string;
+  team: number;
+  score: number;
+  winner: boolean;
+  tournament_id: number;
 };
