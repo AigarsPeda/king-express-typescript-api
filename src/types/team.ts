c1b551e2668e5517c54034228cd98e78133e46f8
Aigars Peda <aigarspeda@gmail.com>
2021-05-15 21:17:53 +0300

    Update team.ts

--- src/types/team.ts
+++ src/types/team.ts
@@ -1,11 +1,20 @@
 export type ITeam = {
-  firstPlayer: string;
-  secondPlayer: string;
-  team: number;
-  score: number;
-  winner: boolean;
-  bigPoints: number;
+  name: string;
+  team_number: number;
+  points: number;
+  is_winner: boolean;
+  big_points: number;
+  tournament_id: number;
+  player_id: number;
 };
+
+// big_points: 0;
+// in_tournament_id: 0;
+// is_winner: false;
+// name: "elīna";
+// player_id: 15;
+// points: 0;
+// tournament_id: 4;
 
 export type ITeamDB = {
   team_id: number;
